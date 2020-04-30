47e775ece6cb3949763f80b433a37ac2a105d33b
Richard Cho <richardcho100@gmail.com>
2020-04-29 23:48:20 -0400

    Add controller, model, migration, & routes for User

---
+++ db/migrate/20200430022011_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :avatar
+      t.string :favorite_quote
+
+      t.timestamps
+    end
+  end
+end
