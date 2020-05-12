59078b7055cb7bec99262997b400d1338594e7f0
Richard Cho <richardcho100@gmail.com>
2020-05-11 21:57:18 -0400

    Add current_user & logged_in? methods w/mocked ver of being logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    def current_user
+        #currently mocked version of being logged in
+        User.first
+    end
+    
+    def logged_in?
+        !!current_user
+    end
 end
