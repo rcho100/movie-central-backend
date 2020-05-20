a513de0021f0cf41257263829bd2ae694e6ebdb3
Richard Cho <richardcho100@gmail.com>
2020-05-19 21:08:07 -0400

    Authenticate pw & login by creating session if successful and send back data

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::SessionsController < ApplicationController
     def create
-        binding.pry
+        user = User.find_by(email: params[:session][:email])
+
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: {
+                error: 'Invalid Credentials'
+            }
+        end
     end
 
-    
 end
