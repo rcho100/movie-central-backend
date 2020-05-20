356864217d3fa2697265ecbe9b91b4e67b017feb
Richard Cho <richardcho100@gmail.com>
2020-05-20 16:53:26 -0400

    Get current user from session & build action to send if logged in

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post "/login", to: "sessions#create"
+      get "/get_current_user", to: "sessions#get_current_user"
       resources :genres
       resources :movies
       resources :users
