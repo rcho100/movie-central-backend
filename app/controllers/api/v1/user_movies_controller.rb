cfdec92470ee6e78e8a650c65014a2e7504d35ec
Richard Cho <richardcho100@gmail.com>
2020-06-19 00:38:30 -0400

    Find/create movie from user_movies_params

--- app/controllers/api/v1/user_movies_controller.rb
+++ app/controllers/api/v1/user_movies_controller.rb
@@ -1,2 +1,19 @@
 class Api::V1::UserMoviesController < ApplicationController
+    def create
+        movie = Movie.find_or_create_by(
+            id_from_api: user_movies_params['id'],
+            title: user_movies_params['title'],
+            backdrop_path: user_movies_params['backdrop_path'],
+            poster_path: user_movies_params['poster_path'],
+            overview: user_movies_params['overview'],
+            tagline: user_movies_params['tagline'],
+            release_date: user_movies_params['release_date'],
+            runtime: user_movies_params['runtime']
+        )    
+    end
+
+    private
+    def user_movies_params
+        params.require(:movDetails).permit(:id, :title, :backdrop_path, :poster_path, :overview, :tagline, :release_date, :runtime)
+    end
 end
