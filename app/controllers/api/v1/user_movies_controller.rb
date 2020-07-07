064b814cf836240e380238cd2323dade1cce320e
Richard Cho <richardcho100@gmail.com>
2020-07-07 15:58:51 -0400

    Send msg when user tries to add when not logged in

--- app/controllers/api/v1/user_movies_controller.rb
+++ app/controllers/api/v1/user_movies_controller.rb
@@ -1,28 +1,35 @@
 class Api::V1::UserMoviesController < ApplicationController
     def create
-        movie = Movie.find_or_create_by(
-            id_from_api: user_movies_params['id'],
-            title: user_movies_params['title'],
-            backdrop_path: user_movies_params['backdrop_path'],
-            poster_path: user_movies_params['poster_path'],
-            overview: user_movies_params['overview'],
-            tagline: user_movies_params['tagline'],
-            release_date: user_movies_params['release_date'],
-            runtime: user_movies_params['runtime']
-        )    
-        if current_user.movies.find{|user_movie| user_movie.id_from_api == movie.id_from_api}
-            resp = {
-                already_in: 'This movie is already in your Watchlist'
-            }
-            render json: resp, status: :ok
-        else
-            current_user.movies << movie
-            options = {
-                include: [:movies]
-            }
+        if logged_in?
+            movie = Movie.find_or_create_by(
+                id_from_api: user_movies_params['id'],
+                title: user_movies_params['title'],
+                backdrop_path: user_movies_params['backdrop_path'],
+                poster_path: user_movies_params['poster_path'],
+                overview: user_movies_params['overview'],
+                tagline: user_movies_params['tagline'],
+                release_date: user_movies_params['release_date'],
+                runtime: user_movies_params['runtime']
+            )    
+            if current_user.movies.find{|user_movie| user_movie.id_from_api == movie.id_from_api}
+                resp = {
+                    already_in: 'This movie is already in your Watchlist'
+                }
+                render json: resp, status: :ok
+            else
+                current_user.movies << movie
+                options = {
+                    include: [:movies]
+                }
 
-            render json: UserSerializer.new(current_user, options), status: :ok
-        end    
+                render json: UserSerializer.new(current_user, options), status: :ok
+            end
+        else
+            resp = {
+                not_logged_in: "You can only add movies to Watchlist if you're logged in"
+            }
+            render json: resp, status: :unauthorized
+        end
     end
 
     private
