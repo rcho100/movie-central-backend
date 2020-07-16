15b8969c4d9405cfbf508028a5150030e6cc29dc
Richard Cho <richardcho100@gmail.com>
2020-07-16 03:13:10 -0400

    Add validations for name & email in signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :movies, through: :user_movies
 
     has_secure_password
+    validates :name, presence: true
+    validates :email, uniqueness: { case_sensitive: false }
+    validates :email, presence: true
+    validates_format_of :email, with: /@/
 end
