a58ee32f508ccf26096aceb52a1adca7967695fa
Richard Cho <richardcho100@gmail.com>
2020-10-01 14:10:38 -0400

    Delete extra '/'

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://moviecentral.netlify.app/'
+    origins 'https://moviecentral.netlify.app'
 
     resource '*',
       headers: :any,
